5da6d5e58c49f38d25b39f27f16cc00c3ac00b21
Fauzan <sbwfauzan@gmail.com>
2026-05-18 23:14:57 +0700

    ulang dri awal dari login sampai hitung nilai

--- main.cpp
+++ main.cpp
@@ -19,7 +19,10 @@ Mahasiswa *tail = NULL;
 // ================= LOGIN =================
 void login() {
     string user, pass;
+
     do {
+        system("cls");
+
         cout << "Selamat Datang di Aplikasi Mahasiswa!" << endl;
         cout << "Ayo Login Terlebih Dahulu!\n";
         cout << "=====================================" << endl;
@@ -28,54 +31,17 @@ void login() {
 
         if (user != "admin" || pass != "123") {
             cout << "Login gagal! Nama atau Password mungkin salah!\n\n";
+            system("pause");
         }
+
     } while (user != "admin" || pass != "123");
 
     cout << "Selamat, Login Telah Sukses!\n";
+    system("pause");
+    system("cls");
 }
 
 // ================= HITUNG NILAI =================
 float hitungNilai(float nilai) {
     return nilai;
-}
-
-// ================= INPUT DATA =================
-void inputData() {
-    int n;
-    cout << "=====================================" << endl;
-    cout << "|      >> Input Mahasiswa >>        |" << endl;
-    cout << "=====================================" << endl;
-    cout << "Ingin Input Berapa Data? ";
-    cin >> n;
-    cin.ignore();
-
-    for (int i = 1; i <= n; i++) {
-        Mahasiswa *baru = new Mahasiswa;
-
-        cout << "\nData ke-" << i << endl;
-        cout << "Nama     : "; getline(cin, baru->nama);
-        cout << "NIM      : "; cin >> baru->nim;
-        cin.ignore();
-        cout << "Jurusan  : "; getline(cin, baru->jurusan);
-        cout << "Nilai    : "; cin >> baru->nilai;
-        cin.ignore();
-
-        baru->nilaiAkhir = hitungNilai(baru->nilai);
-        baru->next = NULL;
-        baru->prev = NULL;
-
-        if (head == NULL) {
-            head = tail = baru;
-            head->next = head;
-            head->prev = head;
-        } else {
-            baru->prev = tail;
-            baru->next = head;
-            tail->next = baru;
-            head->prev = baru;
-            tail = baru;
-        }
-
-        cout << "Selamat, Data Kamu berhasil ditambahkan!\n";
-    }
 }
