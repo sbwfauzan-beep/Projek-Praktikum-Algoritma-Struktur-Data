03d17f88eff605e5223a4404cd2db4eb76646e72
Fauzan <sbwfauzan@gmail.com>
2026-05-18 23:27:46 +0700

    sorting

--- main.cpp
+++ main.cpp
@@ -125,3 +125,150 @@ void tampilData() {
 
     } while (temp != head);
 }
+// ================= SEARCH =================
+
+// --- Sequential Search ---
+void sequentialSearch() {
+
+    system("cls");
+
+    string cari;
+
+    cout << "Masukkan NIM yang dicari: ";
+    cin >> cari;
+
+    if (head == NULL) {
+        cout << "Data kosong!\n";
+        return;
+    }
+
+    Mahasiswa *temp = head;
+    bool ketemu = false;
+    int langkah = 0;
+
+    do {
+
+        langkah++;
+
+        if (temp->nim == cari) {
+
+            cout << "\n===== DATA DITEMUKAN (Sequential Search) =====\n";
+            cout << "Ditemukan pada langkah ke-" << langkah << endl;
+            cout << "Nama        : " << temp->nama << endl;
+            cout << "NIM         : " << temp->nim << endl;
+            cout << "Jurusan     : " << temp->jurusan << endl;
+            cout << "Nilai       : " << temp->nilai << endl;
+            cout << "Nilai Akhir : " << temp->nilaiAkhir << endl;
+
+            ketemu = true;
+            break;
+        }
+
+        temp = temp->next;
+
+    } while (temp != head);
+
+    if (!ketemu)
+        cout << "Data tidak ditemukan!\n";
+}
+
+// --- Binary Search ---
+void binarySearch() {
+
+    system("cls");
+
+    if (head == NULL) {
+        cout << "Data kosong!\n";
+        return;
+    }
+
+    int count = 0;
+    Mahasiswa *temp = head;
+
+    do {
+        count++;
+        temp = temp->next;
+    } while (temp != head);
+
+    Mahasiswa **arr = new Mahasiswa*[count];
+
+    temp = head;
+
+    for (int i = 0; i < count; i++) {
+        arr[i] = temp;
+        temp = temp->next;
+    }
+
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = 0; j < count - i - 1; j++) {
+
+            if (arr[j]->nim > arr[j+1]->nim) {
+
+                Mahasiswa *tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+
+    string cari;
+
+    cout << "Masukkan NIM yang dicari: ";
+    cin >> cari;
+
+    int low = 0, high = count - 1, mid;
+    bool ketemu = false;
+
+    while (low <= high) {
+
+        mid = (low + high) / 2;
+
+        if (arr[mid]->nim == cari) {
+
+            cout << "\n===== DATA DITEMUKAN (Binary Search) =====\n";
+            cout << "Nama        : " << arr[mid]->nama << endl;
+            cout << "NIM         : " << arr[mid]->nim << endl;
+            cout << "Jurusan     : " << arr[mid]->jurusan << endl;
+            cout << "Nilai       : " << arr[mid]->nilai << endl;
+            cout << "Nilai Akhir : " << arr[mid]->nilaiAkhir << endl;
+
+            ketemu = true;
+            break;
+
+        } else if (arr[mid]->nim < cari) {
+
+            low = mid + 1;
+
+        } else {
+
+            high = mid - 1;
+        }
+    }
+
+    if (!ketemu)
+        cout << "Data tidak ditemukan!\n";
+
+    delete[] arr;
+}
+
+void menuSearch() {
+
+    system("cls");
+
+    int pilih;
+
+    cout << "===== Pilihan Searching =====\n";
+    cout << "1. Sequential Search\n";
+    cout << "2. Binary Search (by NIM)\n";
+    cout << "Pilih: ";
+    cin >> pilih;
+
+    if (pilih == 1)
+        sequentialSearch();
+
+    else if (pilih == 2)
+        binarySearch();
+
+    else
+        cout << "Pilihan tidak valid!\n";
+}
